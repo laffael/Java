66801e4dc8cb875ccb7823a5eafd57fcf5eb371b
laffael <13914717717@163.com>
2017-03-05 18:08:36 +0800

    numbers

---
+++ src/Test06.java
@@ -0,0 +1,17 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Raphael Wang
+ * Date: 2017-03-05
+ * Time: 17:58 CST
+ */
+public class Test06 {
+    public static void main(String[] Args) {
+        System.out.println(Byte.MAX_VALUE+"~"+Byte.MIN_VALUE);
+        System.out.println(Byte.parseByte("108")+10);
+        System.out.println(Integer.MAX_VALUE+"~"+Integer.MIN_VALUE);
+        System.out.println(Integer.parseInt("5000")+5);
+        System.out.println(Integer.toBinaryString(16));
+        System.out.println(Integer.toHexString(16));
+    }
+
+}
