dc758f4cf23d800f9662bf9ac4014da47308a29d
laffael <13914717717@163.com>
2017-03-06 12:48:20 +0800

    \usage

---
+++ src/Test11.java
@@ -0,0 +1,16 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Raphael Wang
+ * Date: 2017-03-05
+ * Time: 21:01 CST
+ */
+public class Test11 {
+    public static void main(String[] Args) {
+        System.out.println('\u4e2d'+":");
+        System.out.println(4*Math.pow(16,3)+14* Math.pow(16,2)+2*16+13);
+        System.out.println((char)20013+":"+20013);
+        System.out.println();
+
+    }
+
+}
